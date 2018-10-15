d4d830d4c5822a6ff2db2165c0a14660d0904dc0
duducat531 <wangyining531@gmail.com>
2018-10-16 00:07:55 +0800

    minor refactor

--- Kafka/productor.py
+++ Kafka/productor.py
@@ -1,10 +1,8 @@
 # MetricCollector
 from kafka import KafkaProducer
 
-
-
 topicName = 'ebay_metric'
 serverAddr = 'localhost:9092'
 producer = KafkaProducer(bootstrap_servers=serverAddr, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-logData = {}
-producer.send(topicName, logData)
+metricData = {}
+producer.send(topicName, metricData)
